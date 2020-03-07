8f9866bf7d8a392e47a5c11ba81c5160174db6ac
Guruh Edi Purwanto <guruhedipurwanto@gmail.com>
2020-03-07 09:44:55 +0800

    added default value for user membership

--- models/user.js
+++ models/user.js
@@ -6,7 +6,12 @@ let User = new Schema({
   lastname: { type: String, required: true },
   password: { type: String, required: true },
   username: { type: String, required: true },
-  membership: { type: String, required: true, enum: ["Member", "Not Member"] }
+  membership: {
+    type: String,
+    required: true,
+    enum: ["Member", "Not Member"],
+    default: "Not Member"
+  }
 });
 
 module.exports = mongoose.model("User", User);
