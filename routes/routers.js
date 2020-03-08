74aef0205a27d258571e623bf16591c549a1288e
Guruh Edi Purwanto <guruhedipurwanto@gmail.com>
2020-03-08 15:21:11 +0800

    Added handler to create new messages and its views

--- routes/routers.js
+++ routes/routers.js
@@ -5,6 +5,7 @@ const { check, validationResult } = require("express-validator");
 const { body } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const Message = require("../models/message");
 
 router.get("/", function(req, res, next) {
   res.render("index", { user: req.user });
@@ -86,6 +87,41 @@ router.post(
     } else {
       User.findByIdAndUpdate(req.user._id, { membership: "Member" }, err => {
         if (err) return next(err);
+        res.redirect("/");
+      });
+    }
+  }
+);
+
+router.get("/newmessage", function(req, res, next) {
+  if (req.user) res.render("newmessage");
+  else res.redirect("/");
+});
+
+router.post(
+  "/newmessage",
+  [
+    body("*")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Field can't be empty")
+      .escape()
+  ],
+  function(req, res, next) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("newmessage", { errors: errors.array() });
+    } else {
+      let message = new Message({
+        title: req.body.title,
+        text: req.body.title,
+        user: req.user._id
+      });
+
+      message.save(err => {
+        if (err) next(err);
+
         res.redirect("/");
       });
     }
